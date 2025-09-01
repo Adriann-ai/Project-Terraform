697b27ca49a9358a27d2d51fbff3e23edceef0f0
Adriann-ai <adrian.david1086@gmail.com>
2025-09-01 17:08:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "aws_eks_node_group" "dev" {
   subnet_ids      = aws_subnet.dev_subnet[*].id
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 3
     min_size     = 1
   }
